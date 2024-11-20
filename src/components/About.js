2e8c80a5b7dc71bcf2f6fd4f5d8411f80eaa814d
vantiger69 <vantiger817@gmail.com>
2024-11-20 10:07:50 +0300

    complete_lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About</h2>
+    <p>I made this</p>
+    <img src={image} alt="I made this"/>
+  </div>;
 }
 
 export default About;
